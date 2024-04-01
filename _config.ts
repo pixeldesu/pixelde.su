85b02454ed68b0178237c89bb31ae14137c211ce
Andreas Nedbal <git@pixelde.su>
2024-04-01 23:07:49 +0200

    Install and configure plugins for blogging

--- _config.ts
+++ _config.ts
@@ -6,6 +6,9 @@ import tailwindcss from "lume/plugins/tailwindcss.ts";
 import postcss from "lume/plugins/postcss.ts";
 import minifyHTML from "lume/plugins/minify_html.ts";
 import terser from "lume/plugins/terser.ts";
+import date from "lume/plugins/date.ts";
+import readInfo from "lume/plugins/reading_info.ts";
+import feed from "lume/plugins/feed.ts";
 import nano from "npm:cssnano";
 
 import tailwindOptions from "./tailwind.config.js";
@@ -19,6 +22,8 @@ site.copy("assets/img");
 
 site.use(esbuild());
 site.use(nunjucks());
+site.use(date());
+site.use(readInfo());
 site.use(sitemap());
 site.use(tailwindcss({
   options: tailwindOptions,
@@ -26,6 +31,19 @@ site.use(tailwindcss({
 site.use(postcss());
 site.use(minifyHTML());
 site.use(terser());
+
+site.use(feed({
+  output: ["/blog/posts.rss", "/blog/posts.json"],
+  query: "type=blog index=true",
+  info: {
+    title: "pixeldesu's blog",
+    description: "Personal and technical ramblings and write-ups from pixeldesu",
+  },
+  items: {
+    title: "=title",
+    description: "=excerpt",
+  },
+}));
 
 site.hooks.addPostcssPlugin(nano);
 
