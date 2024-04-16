8b488d15ccade055340317ef89b8bddefd926c51
Andreas Nedbal <git@pixelde.su>
2024-04-17 01:46:16 +0200

    Add JS to randomly show Webring

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -9,4 +9,15 @@ if (navigationToggle) {
 
 document.addEventListener("DOMContentLoaded", () => {
   document.documentElement.classList.remove("no-js");
+
+  if (document.querySelector("[data-webring-container]")) {
+    const webrings = Array.from(document.querySelectorAll("[data-webring]"));
+    const randomWebringIndex = Math.floor(Math.random() * webrings.length);
+
+    const randomWebring = webrings[randomWebringIndex].cloneNode(true);
+    randomWebring.classList.remove("bg-slate-100");
+
+    const webringPlaceholder = document.querySelector("[data-webring-placeholder]");
+    webringPlaceholder.replaceWith(randomWebring);
+  }
 });
