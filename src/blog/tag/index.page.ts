4c88abac1dc8ed1b26ff2e8de9d7fdbde23b3f90
Andreas Nedbal <git@pixelde.su>
2024-07-25 23:08:52 +0200

    Add tag overview pages for blog posts

---
+++ src/blog/tag/index.page.ts
@@ -0,0 +1,19 @@
+export const layout = "layouts/tags/blog.njk"
+
+export default function* ({ tagData, search }) {
+    const posts = search.pages("type=blog post_draft=false", "order date=desc");
+
+    const tags = Array.from(posts)
+        .map((post) => post.tags)
+        .flat()
+        .filter((value, index, array) => array.indexOf(value) === index)
+
+    for (const tag of tags) {
+        yield {
+            url: `/blog/tag/${tag}/`,
+            title: tagData.global[tag]?.name ? `All posts tagged with ${tagData.global[tag].name}` : `All posts tagged with ${tag}`,
+            articles: search.pages(`type=blog post_draft=false ${tag}`, "order date=desc"),
+            tag
+        }
+    }
+}
