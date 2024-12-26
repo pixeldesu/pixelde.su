a8a1dcba12557927ecc84a2121357b9587728a18
Andreas Nedbal <git@pixelde.su>
2024-12-26 02:07:42 +0100

    Add several spin animation variants

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,9 +17,26 @@ export default {
       ]);
     }),
   ],
+  theme: {
+    extend: {
+      animation: {
+        "spin-speed-1x": "spin 15s linear infinite",
+        "spin-speed-2x": "spin 20s linear infinite",
+        "spin-speed-3x": "spin 25s linear infinite",
+        "spin-speed-4x": "spin 30s linear infinite",
+        "spin-speed-5x": "spin 35s linear infinite",
+      },
+    },
+  },
   safelist: [
     "text-6xl",
     "opacity-50",
     "h-fit",
+    "motion-safe:animate-spin-speed-1x",
+    "motion-safe:animate-spin-speed-2x",
+    "motion-safe:animate-spin-speed-3x",
+    "motion-safe:animate-spin-speed-4x",
+    "motion-safe:animate-spin-speed-5x",
+    "origin-center",
   ],
 };
