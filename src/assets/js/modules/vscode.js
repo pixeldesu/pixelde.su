cc5e1522b50e4ffd8389ec8a327d58454e4de9fc
Andreas Nedbal <git@pixelde.su>
2025-02-24 23:41:55 +0100

    Use `globalThis` instead of `window`

--- src/assets/js/modules/vscode.js
+++ src/assets/js/modules/vscode.js
@@ -1,7 +1,7 @@
 const vscodeTargets = document.querySelectorAll("[data-vscode-file]");
 vscodeTargets.forEach((target) => {
   target.addEventListener("click", (event) => {
-    window.location.href =
+    globalThis.location.href =
       `vscode://file/${event.currentTarget.dataset.vscodeFile}`;
   });
 });
