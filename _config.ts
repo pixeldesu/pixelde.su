e711c6fc4bcfd624dc39d19994a4a0a1f9dfdb9b
Andreas Nedbal <git@pixelde.su>
2025-03-15 01:16:51 +0100

    Remove template event time filters

--- _config.ts
+++ _config.ts
@@ -207,18 +207,6 @@ site.filter(
 );
 
 site.filter(
-  "filterEventInFuture",
-  (array: (Partial<{ date: { end: Date } }>)[]) =>
-    array.filter((event) => event.date!.end > (new Date())),
-);
-
-site.filter(
-  "filterEventInPast",
-  (array: (Partial<{ date: { start: Date } }>)[]) =>
-    array.filter((event) => event.date!.start < (new Date())),
-);
-
-site.filter(
   "removeTrailingSlash",
   (url: string) => url.replace(/\/$/, ""),
 );
