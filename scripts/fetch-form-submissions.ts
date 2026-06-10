7d3a4e9a8ced40b4e549c8fc5179af23a8213090
Andreas Nedbal <github-bf215181b5140522137b3d4f6b73544a@desu.email>
2026-06-10 22:56:40 +0200

    Make form fetching script executable directly

--- scripts/fetch-form-submissions.ts
+++ scripts/fetch-form-submissions.ts
@@ -1,22 +1,22 @@
-export default async function () {
-  const API_ROOT = "https://api.netlify.com/api/v1";
-  const FORM_ID = Deno.env.get("NETLIFY_FORM");
-  const NETLIFY_TOKEN = Deno.env.get("NETLIFY_TOKEN");
+const API_ROOT = "https://api.netlify.com/api/v1";
+const FORM_ID = Deno.env.get("NETLIFY_FORM");
+const NETLIFY_TOKEN = Deno.env.get("NETLIFY_TOKEN");
 
-  type FormSubmission = {
-    name: string;
-    summary: string;
-    created_at: string;
-  };
+type FormSubmission = {
+  name: string;
+  summary: string;
+  created_at: string;
+};
 
-  type GuestbookEntry = {
-    name: string;
-    message: string;
-    date: string;
-  };
+type GuestbookEntry = {
+  name: string;
+  message: string;
+  date: string;
+};
 
-  const guestbookEntries: GuestbookEntry[] = [];
+const guestbookEntries: GuestbookEntry[] = [];
 
+try {
   const response = await fetch(
     `${API_ROOT}/forms/${FORM_ID}/submissions`,
     {
@@ -28,16 +28,24 @@ export default async function () {
   );
   const json = await response.json();
 
-  json.forEach((entry: FormSubmission) => {
-    guestbookEntries.push({
-      name: entry.name.length > 0 ? entry.name : "Anonymous",
-      message: entry.summary,
-      date: entry.created_at,
+  if (json) {
+    json.forEach((entry: FormSubmission) => {
+      guestbookEntries.push({
+        name: entry.name.length > 0 ? entry.name : "Anonymous",
+        message: entry.summary,
+        date: entry.created_at,
+      });
     });
-  });
 
-  Deno.writeTextFile(
-    `./src/_data/guestbook.json`,
-    JSON.stringify(guestbookEntries),
-  );
+    Deno.writeTextFile(
+      `./src/_data/guestbook.json`,
+      JSON.stringify(guestbookEntries),
+    );
+  }
+  else {
+    console.log("Form submissions fetched successfully, result object was empty?");
+  }
+}
+catch (e: any) {
+  console.log(`Error while fetching form submissions: ${e.message}`);
 }
